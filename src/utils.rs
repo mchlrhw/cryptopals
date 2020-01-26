ceaa5b64c96051580bd9fd1f5545afb6d469d8b2
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2020-01-26 14:33:28 +0000

    Parallelise detect_single_byte_xor

--- src/utils.rs
+++ src/utils.rs
@@ -1,15 +1,22 @@
+use std::sync::mpsc::channel;
+
+use rayon::prelude::*;
+
 use crate::ciphers::single_byte_xor;
 use crate::heuristics::looks_like_english;
 
 pub(crate) fn detect_single_byte_xor(lines: Vec<&[u8]>) -> Option<Vec<u8>> {
-    for ciphertext in lines {
+    let (sender, receiver) = channel();
+    lines.par_iter().for_each_with(sender, |s, c| {
         for key in 0..=255 {
-            let plaintext = single_byte_xor(ciphertext, key);
+            let plaintext = single_byte_xor(c, key);
             if looks_like_english(&plaintext, 0.3).unwrap() {
-                return Some(plaintext);
+                s.send(plaintext).unwrap();
             }
         }
-    }
+    });
 
-    None
+    let mut res: Vec<_> = receiver.iter().collect();
+
+    res.pop()
 }
