bb5ffb53aafe5c5682425191633db76c643c069a
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2020-01-26 20:46:32 +0000

    Use cartesian product to search

--- src/utils.rs
+++ src/utils.rs
@@ -1,22 +1,16 @@
-use std::sync::mpsc::channel;
-
+use itertools::iproduct;
 use rayon::prelude::*;
 
 use crate::ciphers::single_byte_xor;
 use crate::heuristics::looks_like_english;
 
 pub(crate) fn detect_single_byte_xor(lines: Vec<&[u8]>) -> Option<Vec<u8>> {
-    let (sender, receiver) = channel();
-    lines.par_iter().for_each_with(sender, |s, c| {
-        for key in 0..=255 {
-            let plaintext = single_byte_xor(c, key);
-            if looks_like_english(&plaintext, 0.3).unwrap() {
-                s.send(plaintext).unwrap();
-            }
-        }
-    });
-
-    let mut res: Vec<_> = receiver.iter().collect();
-
-    res.pop()
+    iproduct!(lines, 0..=255)
+        .par_bridge()
+        .map(|(ciphertext, key)| {
+            single_byte_xor(ciphertext, key)
+        })
+        .find_any(|plaintext| {
+            looks_like_english(plaintext, 0.3).unwrap()
+        })
 }
