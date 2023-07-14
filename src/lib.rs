de593c0accc4ccc7d9725b38dd329df2f7f53fc5
mike-watson <4028654+mchlrhw@users.noreply.github.com>
2023-07-14 10:56:49 +0100

    Implement set 1 challenge 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,38 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use base64::Engine;
+use itertools::Itertools;
+
+#[derive(Debug, thiserror::Error)]
+enum Error {
+    #[error("{pair} is not a valid hex character pair")]
+    InvalidHexChunkSize { pair: String },
+
+    #[error(transparent)]
+    InvalidHexPair(#[from] std::num::ParseIntError),
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+fn hex_to_bytes(hex: &str) -> Result<Vec<u8>> {
+    hex.chars()
+        .chunks(2)
+        .into_iter()
+        .map(|chunk| {
+            let pair: String = chunk.collect();
+            if pair.len() != 2 {
+                Err(Error::InvalidHexChunkSize { pair })
+            } else {
+                Ok(u8::from_str_radix(&pair, 16)?)
+            }
+        })
+        .collect()
+}
+
+fn bytes_to_base64(bytes: &[u8]) -> String {
+    base64::engine::general_purpose::STANDARD.encode(bytes)
+}
+
+fn hex_to_base64(hex: &str) -> Result<String> {
+    Ok(bytes_to_base64(&hex_to_bytes(hex)?))
 }
 
 #[cfg(test)]
@@ -7,8 +40,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn hex_to_base64_works() -> anyhow::Result<()> {
+        let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+        let expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+
+        let base64 = hex_to_base64(hex)?;
+
+        assert_eq!(base64, expected);
+
+        Ok(())
     }
 }
